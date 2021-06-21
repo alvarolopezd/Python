f3955f214f90241fa32b29d1bb5964f47e2d179b
Alvaro Lopez Diaz <alodiz@gmail.com>
2021-06-21 21:23:01 +0200

    Tipos de datos y variables

---
+++ PythonCourse/datatype.py
@@ -0,0 +1,53 @@
+#strings
+print("Hello World")
+print('Hello World')
+print('''Hello World''')
+print("""Hello World""")
+
+#Tipo de dato
+print(type("Hello World"))
+
+#Concatenacion
+print("Bye" + "World")
+
+
+#numbers
+print(30+10)
+#integer
+print(type(40.5))
+#float
+print(type(40))
+
+
+#boolean
+True
+False
+print(type(True))
+
+
+#list - lista de datos / array (son datos que pueden cambiar)
+[10, 20, 30, 40]
+["Hola", "Adios"]
+print(type([10, "Hola", True]))
+
+
+#Tuples - tupla (lista que no puede ser cambiada)
+(10, 20, 30, 40)
+print(type((10, 20)))
+
+
+#dictionaries - key : value (clave : valor)
+{"nombre":"Alvaro",
+"apellido":"Lopez",
+"edad":22
+}
+
+print(type({"nombre":"Alvaro",
+"apellido":"Lopez",
+"edad":22
+}))
+
+
+#tipo de dato None (Nada)
+None
+print(type(None))
---
+++ PythonCourse/variables.py
@@ -0,0 +1,14 @@
+name="Alvaro"
+age=22
+print(name)
+print(age)
+
+nombre, edad="Alvaro",22
+print(nombre)
+print(edad)
+
+#Convenctiones
+book_name="H" #Snake Case
+bookName="R" #Camel CAse
+BookName="T" #Pascal Case
+PI=3.14
