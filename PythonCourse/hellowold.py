af450f17e2cebe0901c9e5aa22e7c46654a70834
Alvaro Lopez Diaz <alodiz@gmail.com>
2021-06-21 20:21:26 +0200

    Hello World

---
+++ PythonCourse/hellowold.py
@@ -0,0 +1,4 @@
+# WINDOWS + R -> cmd -> cd/Desktop/Python-course -> python -> helloworld.py (si tecleo tab pone el nombre .py que haya en esa carpeta)
+#cls para limpiar la pantalla de cmd
+#Imprime por pantalla
+print("Hello World")
